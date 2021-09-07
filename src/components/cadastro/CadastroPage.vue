14a330e345752ceb134990a600e646cccd137990
guikoblitz <guilhermerk_contato@hotmail.com>
2021-09-07 11:34:29 -0300

    feat: CadastroPage e PacotesPage

---
+++ src/components/cadastro/CadastroPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="row items-center justify-evenly" color="warning">
+    <q-input outlined color="secondary" v-model="teste" />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class CadastroPage extends Vue {
+  teste = 'Testando 12345...';
+}
+</script>
---
+++ src/components/pacotes/PacotesPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="row items-center justify-evenly" color="warning">
+    <q-input outlined color="primary" v-model="teste" />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class PacotesPage extends Vue {
+  teste = '';
+}
+</script>
