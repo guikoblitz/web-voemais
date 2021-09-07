25f93c7672e654caecf38e5b745a513fac2b7b72
guikoblitz <guilhermerk_contato@hotmail.com>
2021-09-06 23:08:33 -0300

    feat: drawer funcional

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,132 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <!-- <q-layout view="lHh Lpr lFf"> -->
+  <q-layout view="hHh LpR lfr">
     <q-header elevated>
       <q-toolbar>
-        <!-- <q-btn
+        <div class="q-pa-none q-ma-none" v-if="!mini" style="width: 200px">
+          <q-item
+            class="text-white"
+            style="justify-content: center"
+            dense
+            clickable
+          >
+            <q-item-section avatar>
+              <q-img src="logo_raw.png" />
+            </q-item-section>
+          </q-item>
+        </div>
+        <q-separator v-if="!mini" vertical inset class="separador" />
+        <q-btn
           flat
           dense
           round
           icon="menu"
           aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        /> -->
+          @click="mini = !mini"
+        />
 
-        <q-toolbar-title>
-          Voe+
-        </q-toolbar-title>
+        <q-toolbar-title> Voe+ </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <!-- <q-drawer
-      v-model="leftDrawerOpen"
+    <q-drawer
+      v-model="leftDrawer"
       show-if-above
       bordered
-      content-class="bg-grey-1"
+      :mini="mini"
+      content-class="background-color-drawer"
+      class="text-white"
+      :width="215"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
+        <div>
+          <q-expansion-item
+            ref="menuInicio"
+            group="menuLateral"
+            class="esconder-seta-expansion-item"
+            hide-dropdown-icon
+            label="Início"
+            icon="home"
+            @click="teste('Inicio')"
+          >
+          </q-expansion-item>
+          <q-tooltip
+            v-if="mini"
+            anchor="center right"
+            self="center left"
+            content-style="font-size: 14px"
+          >
+            <strong>Início</strong>
+          </q-tooltip>
+        </div>
+        <hr
+          style="
+            border: 0;
+            border-top: 1px solid rgba(0, 0, 0, 0.12);
+            heigth: 1px;
+          "
+          class="q-ma-none"
+        />
+        <div>
+          <q-expansion-item
+            ref="menuConfiguracoes"
+            group="menuLateral"
+            class="esconder-seta-expansion-item"
+            hide-dropdown-icon
+            label="Cadastro"
+            icon="settings"
+            @click="teste('Cadastro')"
+          >
+          </q-expansion-item>
+          <q-tooltip
+            v-if="mini"
+            anchor="center right"
+            self="center left"
+            content-style="font-size: 14px"
+          >
+            <strong>Cadastro</strong>
+          </q-tooltip>
+        </div>
+        <hr
+          style="
+            border: 0;
+            border-top: 1px solid rgba(0, 0, 0, 0.12);
+            heigth: 1px;
+          "
+          class="q-ma-none"
+        />
+        <div>
+          <q-expansion-item
+            ref="menuConfiguracoes"
+            group="menuLateral"
+            class="esconder-seta-expansion-item"
+            hide-dropdown-icon
+            label="Pacotes"
+            icon="flight_takeoff"
+            @click="teste('Pacotes')"
+          >
+          </q-expansion-item>
+          <q-tooltip
+            v-if="mini"
+            anchor="center right"
+            self="center left"
+            content-style="font-size: 14px"
+          >
+            <strong>Pacotes</strong>
+          </q-tooltip>
+        </div>
+        <hr
+          style="
+            border: 0;
+            border-top: 1px solid rgba(0, 0, 0, 0.12);
+            heigth: 1px;
+          "
+          class="q-ma-none"
         />
       </q-list>
-    </q-drawer> -->
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,60 +135,25 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({
-  components: { EssentialLink }
-})
+@Component({})
 export default class MainLayout extends Vue {
-  leftDrawerOpen = false;
-  essentialLinks = linksData;
+  leftDrawer = true;
+  mini = true;
+
+  teste(tab: string): void {
+    console.log(tab);
+  }
 }
 </script>
+
+<style>
+.esconder-seta-expansion-item .q-expansion-item__toggle-icon {
+  display: none;
+}
+
+.background-color-drawer {
+  background-color: #243665;
+}
+</style>
