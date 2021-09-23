a5a3cbf6660997a5e2f61dba049d8928ce6de402
guikoblitz <guilhermerk_contato@hotmail.com>
2021-09-23 20:37:20 -0300

    fix: ajustes eslint e novas dependencias

--- .eslintrc.js
+++ .eslintrc.js
@@ -83,6 +83,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
 
     // allow debugger during development only
     allow: ['arrowFunctions'],
---
+++ src/components/currency/VueCurrency.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-input
+    ref="inputRef"
+    dense
+    outlined
+    type="text"
+    label="Valor do Pacote (R$)"
+    :value="formattedValue"
+    @keyup.enter="$event.target.blur()"
+  />
+</template>
+
+<script>
+import { useCurrencyInput } from 'vue-currency-input';
+
+export default {
+  name: 'CurrencyInput',
+  props: {
+    value: Number, // Vue 2: value
+    options: Object
+  },
+  setup(props) {
+    const { formattedValue, inputRef } = useCurrencyInput(props.options);
+
+    return { inputRef, formattedValue };
+  }
+};
+</script>
