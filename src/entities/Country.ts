3bdb0e38a10ce4942c6c6bd3dc247187ad5de498
guikoblitz <guilhermerk_contato@hotmail.com>
2021-09-23 21:56:42 -0300

    feat: novas entidades

---
+++ src/entities/Country.ts
@@ -0,0 +1,9 @@
+export class Country {
+  id_country: string;
+
+  name_country: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
---
+++ src/entities/TravelPackageType.ts
@@ -0,0 +1,9 @@
+export class TravelPackageType {
+  id_travel_package_type: string;
+
+  travel_package_type: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
