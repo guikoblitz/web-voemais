ef1fb3163b3e6f27f157bbcc354c3f14b5b94613
guikoblitz <guilhermerk_contato@hotmail.com>
2021-10-15 23:04:53 -0300

    feat: nova entidade User

---
+++ src/entities/User.ts
@@ -0,0 +1,21 @@
+export class User {
+  id_user: string;
+
+  cpf: string;
+
+  name: string;
+
+  last_name: string;
+
+  email: string;
+
+  employee: boolean;
+
+  date_birth: Date;
+
+  password: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
