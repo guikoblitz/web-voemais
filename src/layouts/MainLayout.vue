190c2bb7cdbb0ae81570df18c7efe38cec361db6
guikoblitz <guilhermerk_contato@hotmail.com>
2021-10-20 23:37:05 -0300

    fix: formatacao arquivos

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,61 +1,32 @@
 <template>
-  <q-layout view="hHh LpR lfr">
-    <q-header elevated>
-      <q-toolbar class="q-pl-none">
-        <div class="q-pa-none q-ma-none">
-          <q-item
-            class="text-white"
-            style="justify-content: center"
-            dense
-            clickable
-            @click="setRouter('/', 'Início')"
-          >
-            <q-item-section avatar class="q-pa-none">
-              <q-img src="logo_raw.png" />
-            </q-item-section>
-          </q-item>
-        </div>
+    <q-layout view="hHh LpR lfr">
+        <q-header elevated>
+            <q-toolbar class="q-pl-none">
+                <div class="q-pa-none q-ma-none">
+                    <q-item class="text-white" style="justify-content: center" dense clickable @click="setRouter('/', 'Início')">
+                        <q-item-section avatar class="q-pa-none">
+                            <q-img src="logo_raw.png" />
+                        </q-item-section>
+                    </q-item>
+                </div>
 
-        <q-toolbar-title> {{ title }} </q-toolbar-title>
+                <q-toolbar-title> {{ title }} </q-toolbar-title>
 
-        <div class="q-pr-sm q-ma-none">
-          <q-btn
-            rounded
-            outline
-            label="Cadastro"
-            size="13px"
-            color="light-blue-3"
-            @click="abrirCadastroUsers = true"
-          />
-        </div>
-        <div class="q-pr-sm q-ma-none">
-          <q-btn
-            rounded
-            outline
-            label="Login"
-            size="13px"
-            color="light-blue-3"
-            @click="abrirLogin = true"
-          />
-        </div>
-      </q-toolbar>
-    </q-header>
+                <div class="q-pr-sm q-ma-none">
+                    <q-btn rounded outline label="Cadastro" size="13px" color="light-blue-3" @click="abrirCadastroUsers = true" />
+                </div>
+                <div class="q-pr-sm q-ma-none">
+                    <q-btn rounded outline label="Login" size="13px" color="light-blue-3" @click="abrirLogin = true" />
+                </div>
+            </q-toolbar>
+        </q-header>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-    <Login
-      v-if="abrirLogin"
-      :abrirLogin="abrirLogin"
-      @confirmarLogin="confirmarLogin($event)"
-      @fecharModal="abrirLogin = $event"
-    />
-    <CadastroUsersModal
-      v-if="abrirCadastroUsers"
-      :abrirCadastroUsers="abrirCadastroUsers"
-      @closeModal="abrirCadastroUsers = $event"
-    />
-  </q-layout>
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+        <Login v-if="abrirLogin" :abrirLogin="abrirLogin" @confirmarLogin="confirmarLogin($event)" @fecharModal="abrirLogin = $event" />
+        <CadastroUsersModal v-if="abrirCadastroUsers" :abrirCadastroUsers="abrirCadastroUsers" @closeModal="abrirCadastroUsers = $event" />
+    </q-layout>
 </template>
 
 <script lang="ts">
@@ -67,49 +38,49 @@ import CadastroUsersModal from 'src/components/users/CadastroUsersModal.vue';
 
 @Component({ components: { Login, CadastroUsersModal } })
 export default class MainLayout extends Vue {
-  leftDrawer = true;
-  mini = true;
-  title = 'Início';
-  abrirLogin = false;
-  abrirCadastroUsers = false;
+    leftDrawer = true;
+    mini = true;
+    title = 'Início';
+    abrirLogin = false;
+    abrirCadastroUsers = false;
 
-  setRouter(path: string, tab: string): void {
-    if (this.title !== tab) {
-      Loading.show({ message: `Carregando ${tab}...` });
-      this.title = tab;
-      this.mini = true;
-      this.$router.push(path).catch(() => {});
-      Loading.hide();
-    } else {
-      this.mini = !this.mini;
+    setRouter(path: string, tab: string): void {
+        if (this.title !== tab) {
+            Loading.show({ message: `Carregando ${tab}...` });
+            this.title = tab;
+            this.mini = true;
+            this.$router.push(path).catch(() => {});
+            Loading.hide();
+        } else {
+            this.mini = !this.mini;
+        }
     }
-  }
 
-  confirmarLogin(retorno: boolean): void {
-    if (retorno) {
-      if (this.title !== 'Início') {
-        Loading.show({ message: 'Carregando Início...' });
-        this.title = 'Início';
-        this.mini = true;
-        this.$router.push('/').catch(() => {});
-        Loading.hide();
-        notificarSucesso('Login efetuado com sucesso.');
-      } else {
-        notificarSucesso('Login efetuado com sucesso.');
-      }
-    } else {
-      notificarErro('Houve um erro ao efetuar login.');
+    confirmarLogin(retorno: boolean): void {
+        if (retorno) {
+            if (this.title !== 'Início') {
+                Loading.show({ message: 'Carregando Início...' });
+                this.title = 'Início';
+                this.mini = true;
+                this.$router.push('/').catch(() => {});
+                Loading.hide();
+                notificarSucesso('Login efetuado com sucesso.');
+            } else {
+                notificarSucesso('Login efetuado com sucesso.');
+            }
+        } else {
+            notificarErro('Houve um erro ao efetuar login.');
+        }
     }
-  }
 }
 </script>
 
 <style>
 .esconder-seta-expansion-item .q-expansion-item__toggle-icon {
-  display: none;
+    display: none;
 }
 
 .background-color-drawer {
-  background-color: #243665;
+    background-color: #243665;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,19 +7,19 @@ import routes from './routes';
  * directly export the Router instantiation
  */
 
-export default route(function ({ Vue }) {
-  Vue.use(VueRouter);
+export default route(function({ Vue }) {
+    Vue.use(VueRouter);
 
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
+    const Router = new VueRouter({
+        scrollBehavior: () => ({ x: 0, y: 0 }),
+        routes,
 
-    // Leave these as is and change from quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
-  });
+        // Leave these as is and change from quasar.conf.js instead!
+        // quasar.conf.js -> build -> vueRouterMode
+        // quasar.conf.js -> build -> publicPath
+        mode: process.env.VUE_ROUTER_MODE,
+        base: process.env.VUE_ROUTER_BASE
+    });
 
-  return Router;
-})
+    return Router;
+});
